f39dd2acfb1f8476297d7e84ee23c149794395de
IanCl13 <ianclarke281@gmail.com>
2021-07-23 16:33:35 +0000

    added random tiles

--- index.js
+++ index.js
@@ -5,13 +5,13 @@ let combosFound = 0;
 const colors = [
     'red',
     'green',
-    'blue',
     'orange',
+    'blue',
     'pink',
     'cyan',
     'yellow',
-    'purple'
-];
+    'purple',
+]
 
 const cards = [...document.querySelectorAll(.'card')];
     for (let color of colors ) {
@@ -30,21 +30,50 @@ const cards = [...document.querySelectorAll(.'card')];
          
     }
 
-function onCardClicked (e) {
-    const target = e.currentTarget;
-    console.log (target.className); 
-    target.className = target.className
+function onCardClicked(e) {
+    const target  = e.currentTarget;
+
+    if (
+    preventClick ||
+    target === clickedCard ||
+    target.className.includes('done')
+    ) {
+        return;
+    }
+
+target.className = target.className
     .replace ('color-hidden', '')
     .trim ();
+target.className += ' done';
 
-}
+if (!clickedCard) {
+    clickedCard = target; 
 
 } else if (clickedCard) {
+    preventClick =  true ;
     if (
         clickedCard.getAttribute('data-color') !==
         target.getAttribute('data-color')
     ) {
-        console.log {'cards are not equal'}
+        console.log ('cards are equal'); 
+    } else {
+        console.log ('cards are not equal');
+        setTimeout (() => {
+            console.log ('we are here!');
+            clickedCard.className = 
+                clickedCard.className.replace ('done', '').trim() +
+                'color-hidden';
+            target.className = 
+                target.className.replacce ('done', '').trim() +
+                'color-hidden';
+            clickedCard = null;
+            preventClick = false;
+        }, 500);
+    } else {
+        combosFound++;
+        clickedCard=null;
+        if (combosFound === 8)
+            alert('YOU WIN!')
     }
 
     }
