8b0c1eee5013de20934fbba2a2e2eaade25ef807
IanCl13 <ianclarke281@gmail.com>
2021-07-23 13:00:09 +0000

    changed colors

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 function onCardClicked (e) {
     const target = e.currentTarget;
     console.log (target.className); 
-    target.className = target.className.replace
+    target.className = target.className
+    .replace ('color-hidden', '')
+    .trim ();
 }
