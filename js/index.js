9dcb6214880bb5d3b7c140280f119a3086acfd37
IanCl13 <ianclarke281@gmail.com>
2021-07-24 16:35:25 +0000

    fixed error where game would stop after one match

--- js/index.js
+++ js/index.js
@@ -15,19 +15,16 @@ const colors = [
 
 const cards = [...document.querySelectorAll('.card')];
     for (let color of colors ) {
-        const cardAIndex = parseInt(Math.randon() * cards.length);
+        const cardAIndex = parseInt(Math.random() * cards.length);
         const cardA = cards[cardAIndex];
         cards.splice(cardAIndex, 1);
-        cardA.className += ` ${color}`;
-        cardA.setAttribute('data-color, color');
+        cardA.setAttribute('data-color', color);
 
         
-        const cardBIndex = parseInt(Math.randon() * cards.length);
+        const cardBIndex = parseInt(Math.random() * cards.length);
         const cardB = cards[cardBIndex];
         cards.splice(cardBIndex, 1);
-        cardB.className += ` ${color}`;
-        cardB.setAttribute('data-color, color');
-         
+        cardB.setAttribute('data-color', color);         
     }
 
 function onCardClicked(e) {
@@ -52,29 +49,30 @@ if (!clickedCard) {
 } else if (clickedCard) {
     preventClick =  true ;
     if (
-        clickedCard.getAttribute('data-color') !==
+        clickedCard.getAttribute('data-color') ===
         target.getAttribute('data-color')
     ) {
         console.log ('cards are equal'); 
-    } else {
-        console.log ('cards are not equal');
-        setTimeout (() => {
-            console.log ('we are here!');
-            clickedCard.className = 
-                clickedCard.className.replace ('done', '').trim() +
-                'color-hidden';
-            target.className = 
-                target.className.replacce ('done', '').trim() +
-                'color-hidden';
-            clickedCard = null;
-            preventClick = false;
-        }, 500);
-    } else {
+
         combosFound++;
         clickedCard=null;
+        preventClick=false;
         if (combosFound === 8)
             alert('YOU WIN!')
         }
-
+        else {
+            console.log ('cards are not equal');
+            setTimeout (() => {
+                console.log ('we are here!');
+                clickedCard.className = 
+                    clickedCard.className.replace ('done', '').trim() +
+                    ' color-hidden';
+                target.className = 
+                    target.className.replace ('done', '').trim() +
+                    ' color-hidden';
+                clickedCard = null;
+                preventClick = false;
+            }, 500);
+        }
     }
 }
